7364feca528e7892f66320497cd0501f663fa8ed
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2021-02-04 02:42:31 +0700

    Heroku Setting

--- src/main/java/com/codegym/project/controller/AuthController.java
+++ src/main/java/com/codegym/project/controller/AuthController.java
@@ -5,16 +5,14 @@ import com.codegym.project.model.user.User;
 import com.codegym.project.service.jwt.JwtService;
 import com.codegym.project.service.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("*")
@@ -25,6 +23,11 @@ public class AuthController {
     private JwtService jwtService;
     @Autowired
     private IUserService iUserService;
+
+    @GetMapping("/")
+    public ResponseEntity<Iterable<User>> test() {
+        return new ResponseEntity<>(iUserService.findAll(), HttpStatus.OK);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User user) {
